baac5e8cf83edacdc7578308a361147789ae9e44
Fitzy <fitzystrikesagain@gmail.com>
2021-06-05 18:19:07 -0500

    cleanup

--- sql-and-data-modeling/psycopg-practice-2.py
+++ sql-and-data-modeling/psycopg-practice-2.py
@@ -1,19 +1,15 @@
-from utils.constants import get_conn
-# Create a new table
-
 """
 Exercise 2
 Now modify your script to use string composition throughout. Use both `%s` and `%(named_var)s` methods.
 Run the script and then connect to the database using psql to inspect that the records were properly inserted into 
 the tables in our database.
 """
-
 from utils.constants import get_conn
 
 TABLE_NAME = "vehicles"
-columns = ["year", "make", "model", "mileage"]
-types = ["int", "varchar", "varchar", "float"]
-values = """
+COLUMNS = ["year", "make", "model", "mileage"]
+TYPES = ["int", "varchar", "varchar", "float"]
+VALUES = """
 (1994, 'Ford', 'Pinto', 18),
 (1997, 'Chevy', 'Malibu', 23),
 (1990, 'Nissan', '300ZX', 16),
@@ -23,27 +19,27 @@ values = """
 
 def main():
     """
-    Creates a vehicles table, inserts some values, then queries and prints the results
+    Creates a vehicles table, inserts some VALUES, then queries and prints the results
     :return: None
     """
     create_table = f"""
     DROP TABLE IF EXISTS {TABLE_NAME};
     CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
-        {columns[0]} {types[0]},
-        {columns[1]} {types[1]},
-        {columns[2]} {types[2]},
-        {columns[3]} {types[3]}
+        {COLUMNS[0]} {TYPES[0]},
+        {COLUMNS[1]} {TYPES[1]},
+        {COLUMNS[2]} {TYPES[2]},
+        {COLUMNS[3]} {TYPES[3]}
     )
     """
 
-    insert_values = f"INSERT INTO {TABLE_NAME} VALUES{values}"
+    insert_values = f"INSERT INTO {TABLE_NAME} VALUES {VALUES}"
 
     select_all = f"SELECT * FROM {TABLE_NAME}"
 
     # Retriever cursor from the helper
     conn, cur = get_conn()
 
-    # Create table and insert values
+    # Create table and insert VALUES
     cur.execute(create_table)
     cur.execute(insert_values)
 
