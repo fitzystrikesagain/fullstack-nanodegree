c28f357874c44d665c91068c0d68d3ca9dbc7cd4
Fitzy <fitzystrikesagain@gmail.com>
2021-06-11 23:01:25 -0500

    create migration to add completed column

--- sql-and-data-modeling/dummy_todo.py
+++ sql-and-data-modeling/dummy_todo.py
@@ -20,7 +20,7 @@ class Todo(db.Model):
     __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
-    completed = db.Column(db.Boolean, nullable=True, default=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr(self):
         return f"<Todo id: {self.id}, description: {self.description}>"
