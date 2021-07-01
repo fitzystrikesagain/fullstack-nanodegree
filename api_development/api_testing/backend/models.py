e05ceca73644c29d6cc32f306cb971a8ad4bc4a4
Fitzy <fitzystrikesagain@gmail.com>
2021-06-30 21:31:51 -0500

    cleanup and start testing exercise

---
+++ api_development/api_testing/backend/models.py
@@ -0,0 +1,63 @@
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+
+pg_dbname = os.environ.get("POSTGRES_DB")
+pg_user = os.environ.get("POSTGRES_USER")
+pg_pass = os.environ.get("POSTGRES_PASSWORD")
+pg_host = f"{os.environ.get('POSTGRES_HOST')}:5432"
+pg_host_local = "localhost:5432"
+database_path = f"postgresql://{pg_user}:{pg_pass}@{pg_host}/{pg_dbname}"
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Movie
+
+'''
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    rating = Column(Integer)
+
+    def __init__(self, title, author, rating):
+        self.title = title
+        self.author = author
+        self.rating = rating
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'rating': self.rating,
+        }
